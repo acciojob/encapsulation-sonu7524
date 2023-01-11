7fa5f718a297ad9221682ea7700406096533bdfd
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2023-01-11 18:41:53 +0530

    update as given situation in problem

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        //calling setter function name setName(String_name) to store name string
+        rw.setName("Sonu Daryani");
+        //print the string usnig getter method name getName()
+        System.out.println(rw.getName());
+    }
 }
