dd26c58ad5b4ae1b6264f93b35df84617524471f
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2023-01-11 18:42:34 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    // if we direct access name it shows error that java: name has private access in com.driver.RWOnly
+    // in order to resolve the issue create a getter & setter function
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
